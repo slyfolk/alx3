8ccd93807ef55cc5f11ba4f02726113db4a10397
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2023-01-04 14:43:23 -0800

    create task 12-fuzzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -15,4 +15,4 @@ def fizzbuzz():
         elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{} ".format(number), end=""
+            print("{} ".format(number), end="")
