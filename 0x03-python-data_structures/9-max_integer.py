7d0b5ec7b6de4229da91753f3a465f03adbb41b9
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2023-01-09 14:12:49 -0800

    created task 9 - find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    maximum = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > maximum:
+            maximum = my_list[i]
+
+    return (maximum)
